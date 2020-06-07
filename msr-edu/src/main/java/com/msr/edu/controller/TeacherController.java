27e8e692c98e3f838cd364b0110e2e9300795fbb
lmttting <linmt.japan@gmail.com>
2020-06-08 08:44:16 +0900

    逻辑删除

--- msr-edu/src/main/java/com/msr/edu/controller/TeacherController.java
+++ msr-edu/src/main/java/com/msr/edu/controller/TeacherController.java
@@ -3,11 +3,11 @@ package com.msr.edu.controller;
 
 import com.msr.edu.entity.Teacher;
 import com.msr.edu.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,17 +19,28 @@ import java.util.List;
  * @author tom
  * @since 2020-06-05
  */
+
+@Api(description="讲师管理")
 @RestController
 @RequestMapping("/edu/teacher")
+@CrossOrigin //跨域
 public class TeacherController {
 
     @Autowired
     private TeacherService teacherService;
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping
     public List<Teacher> list(){
         return teacherService.list(null);
     }
 
+    @ApiOperation(value = "根据ID删除讲师")
+    @DeleteMapping("{id}")
+    public boolean removeById(
+            @ApiParam(name = "id", value = "讲师ID", required = true)
+            @PathVariable String id){
+        return teacherService.removeById(id);
+    }
 }
 
